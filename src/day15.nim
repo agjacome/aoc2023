171651947d994ecfdea3d287e9aa7bd3a44f8e9f
Alberto G. Jácome <contact@agjacome.dev>
2023-12-15 12:56:26 +0100

    feat: solve day15 part2

--- src/day15.nim
+++ src/day15.nim
@@ -1,21 +1,55 @@
-import std/[math, sequtils, strutils, sugar]
+import std/[math, sequtils, strutils, tables]
 
 type
-    Step = seq[char]
-    Sequence = seq[Step]
+    Sequence = seq[string]
+
+    StepKind = enum Insert, Remove
+    StepAction = object
+        label: string
+        case kind: StepKind:
+            of Insert:
+                focalLength: int
+            of Remove:
+                discard
+
+    Box = OrderedTable[string, int]
+    Boxes = array[256, Box]
 
 func parseSequence(input: string): Sequence =
-    input.strip.split(",").mapIt(it.toSeq)
+    input.strip.split(",")
 
-func hash(step: Step): int =
-    step.foldl(first = 0, operation = (a + b.ord) * 17 mod 256)
+func hash(str: string): int =
+    str.foldl((a + b.ord) * 17 mod 256, 0)
+
+func action(step: string): StepAction =
+    let parts = step.split({'-', '='})
+
+    if parts[1].len > 0:
+        return StepAction(kind: Insert, label: parts[0], focalLength: parts[1].parseInt)
+    else:
+        return StepAction(kind: Remove, label: parts[0])
 
 func verify(sequence: Sequence): int =
     sequence.map(`hash`).sum
 
+func initialize(sequence: Sequence): Boxes =
+    for step in sequence:
+        let action = step.action
+        let index = action.label.hash
+
+        case action.kind:
+            of Insert: result[index][action.label] = action.focalLength
+            of Remove: result[index].del(action.label)
+
+func focusingPower(boxes: Boxes): int =
+    for i, box in boxes:
+        for j, focalLength in box.values.toSeq:
+            result += (i + 1) * (j + 1) * focalLength
+
 func partOne(input: string): string =
     $input.parseSequence.verify
 
-func partTwo(input: string): string = "TODO"
+func partTwo(input: string): string =
+    $input.parseSequence.initialize.focusingPower
 
 const day* = (partOne, partTwo)
